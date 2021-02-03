2d2afc3479eb7df8ad2650553bbc530769793e61
maikthulhu <maik@halfzero.net>
2021-02-02 16:42:47 -0800

    Reading config and parsing config works. Now what?

--- main.py
+++ main.py
@@ -26,6 +26,7 @@ from glob import iglob
 #     ALLOWOVERRIDE = auto()
 #     REQUIRE = auto()
 
+FILE_INCLUDE_HEADER = "# INCLUDED FROM"
 INCLUDE = "INCLUDE"
 
 class Directive:
@@ -40,6 +41,9 @@ class Directive:
         (self.directive, self.directive_args) = line.split(None, 1) # Split once to get directive and args (none means to use default whitespace delimiter)
         self.parent = parent
 
+    def __str__(self):
+        return self.directive
+    
 def read_config(config_dir, server_config_file):
     config = path.join(config_dir, server_config_file)
     if server_config_file.startswith("/"):
@@ -49,7 +53,7 @@ def read_config(config_dir, server_config_file):
     config_lines = []
 
     for fpath in iglob(config):
-        config_lines.append(f"# INCLUDED FROM: {fpath}\n")
+        config_lines.append(f"{FILE_INCLUDE_HEADER}: {fpath}")
         try:
             with open(fpath, 'r') as f:
                 for line in f.readlines():
@@ -58,14 +62,14 @@ def read_config(config_dir, server_config_file):
                         # Ignore lines that start with a #, like this one :|  Also empty lines. Why are you still reading this?
                         continue
                     else:
-                        line_ucase = line_stripped.upper()
-                        if line_ucase.startswith(INCLUDE):
-                            (directive, args) = line.split(None, 1)
-                            args_noquotes = args.strip('"\'\r\n')
+                        line_stripped_ucase = line_stripped.upper()
+                        if line_stripped_ucase.startswith(INCLUDE):
+                            (directive, args) = line_stripped.split(None, 1)
+                            args_noquotes = args.strip('"\'')
                             config_path = args_noquotes
                             config_lines.extend(read_config(config_dir, config_path))
                         else:
-                            config_lines.append(line)
+                            config_lines.append(line_stripped)
 
         except IOError:
             print(f"Error reading config file: {config}.")
@@ -80,10 +84,52 @@ def read_config(config_dir, server_config_file):
                 #             # Close the current directive
                 # else:
                 #     new_directive = Directive(config, line.strip(), new_directive)
-
+    
     return config_lines
 
+def parse_config(config_list):
+    from_file = ""
+    parent = None
+    d = None
+    directives = []
+    
+    # Iterate over each line from the parsed config
+    for line in config_list:
+        # Grep our custom file include directive so we know which file this came from
+        if line.startswith(FILE_INCLUDE_HEADER):
+            # We want to split on ':' for this line, only have 1 match, and strip leading/trailing spaces
+            (_, from_file) = line.split(':', 1)
+            # Then set from_file so we can use it when parsing subsequent directives
+            from_file = from_file.strip()
+            # Then move on to the next line, no more parsing needed
+            continue
+        elif line.startswith("<"):
+            if line.startswith("</"):
+                # This is a closing directive, it should match parent's directive
+                # Yo dawg....
+                parent = parent.parent
+            else:
+                # This is a new parent
+                d = Directive(from_file, line.strip("<>"), parent)
+                # Update reference to current parent
+                parent = d
+                # Stop processing and move on
+                #continue
+        else:
+            # Parse this line, and assign it to parent (if any)
+            d = Directive(from_file, line, parent)
+        
+        # Add the directive to the list....?
+        directives.append(d)
+    
+    return directives
+
 def main():
+    # This script assumes the config is syntactically correct, though doesn't cover every single nuance (e.g. line-continuations with '\')
+    #   If you want to know if the config's syntax is correct, run apachectl -t. I could do it for you, but I'm not gonna. Unless you ask nicely. With Bitcoin.
+    # You might have noticed I'm using recursion to parse Include directives. If you have a circular include, there are no brakes.
+    #   Seriously, check syntax before you begin. Or send more Bitcoin.
+
     parser = argparse.ArgumentParser()
     parser.add_argument("--config_dir", help="location of Apache config dir (default: HTTPD_ROOT=/etc/httpd)", default="/etc/httpd", type=str)
     parser.add_argument("--server_config_file", help="relative path to Apache config file (default: SERVER_CONFIG_FILE=conf/httd.conf)", default="conf/httpd.conf", type=str)
@@ -91,9 +137,11 @@ def main():
 
     lines = read_config(args.config_dir, args.server_config_file)
 
-    print(''.join(lines))
+    #print('\n'.join(lines))
 
+    directives = parse_config(lines)
 
+    return 0
 
 if __name__ == '__main__':
     main()
